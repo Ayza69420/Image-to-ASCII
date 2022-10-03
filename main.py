3da137ed9a72453406827106ffd00171e2e4511c
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-10-03 03:40:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ image_path = f"{directory}/{image_name}"
 img = cv2.imread(image_path)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-edges = cv2.Canny(img, 10, 200)
+edges = cv2.Canny(img, 50, 200)
 
 new_image = f"{directory}/new_image.png"
 
 img = cv2.imwrite(new_image, edges)
 img = cv2.imread(new_image)
 
-rows = ["".join(["#" if all(img[y,x]) else "." for x in range(img.shape[1])]) for y in range(img.shape[0])]
+rows = ["".join(["@" if all(img[y,x]) else "." for x in range(img.shape[1])]) for y in range(img.shape[0])]
 
 if "res.txt" not in os.listdir(directory):
     with open(f"{directory}/res.txt", "x") as file:
